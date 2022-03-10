c322038e49dc75d56371d1ed76d3a598c8d12052
Her_Jeremy <herj043@hsd.k12.or.us>
2022-03-10 15:29:28 -0800

    added if and else

--- research.py
+++ research.py
@@ -2,17 +2,39 @@
 
 
 from tkinter import *
-leave = input("do you wish to shut the program down? (yes/no): ")
+import test as t
+t.cry()
+leave = input("do you wish to to deal with looping windows?  ")
 
-if "y" in leave.lower():
+def loop_win():
+    for i in range(4):
+        print("window loop (" + str(i+1) + "/4)")
+        myLabel = Label(Tk(), text="Hello World")
+
+        myLabel.pack()
+        
+        Tk().mainloop()
+    
+
+if "end" in leave.lower():
     print("Exiting program...")
     exit()
+elif "y" in leave.lower():
+    print("looping windows...")
+    loop_win()
+
+#Spams windows after you deleted the window
+
+
+root = Tk()
+
 for i in range(4):
-    myLabel = Label(Tk(), text="Hello World")
+    myLabel = Label(root, text="Hello World")
 
     myLabel.pack()
-
-
-
+root.config(background="#333333")
+#Setting root.mainloop in loop then deleting breaks it also only includes "Hello World"    
+root.mainloop()
+exit()
 
 
